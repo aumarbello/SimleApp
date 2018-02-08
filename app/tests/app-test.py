4d9e6a78f17e842c51c9793cbf6892e0c9988663
Ahmed Umar <khattabu@gmail.com>
2018-02-08 12:24:06 +0100

    Added more test suites

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -11,6 +11,9 @@ class AddTest(unittest.TestCase):
         self.assertEqual(sum_both(3, 1), 4)
         self.assertEqual(sum_both(1.0, 1), 2)
         self.assertEqual(sum_both(1.2, 2.1), 3.3)
+        self.assertEqual(sum_both(0.5, 2.1), 2.6)
+        self.assertEqual(sum_both(1.2, 0), 1.2)
+        self.assertEqual(sum_both(1.5, 0.6), 2.1)
 
 
 if __name__ == '__main__':
